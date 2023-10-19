1010ccf813397cb84c3c4ba08d0d1d1320b3f97c
francisLaplage <francis_laplage@yahoo.fr>
2023-10-19 12:18:51 +0200

    version build v2

--- src/main/java/fr/njangui_pro/NjanguiProApplication.java
+++ src/main/java/fr/njangui_pro/NjanguiProApplication.java
@@ -1,13 +1,29 @@
 package fr.njangui_pro;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class NjanguiProApplication {
+import fr.njangui_pro.entities.Membre;
+import fr.njangui_pro.repository.MembreRepository;
 
+@SpringBootApplication
+public class NjanguiProApplication implements CommandLineRunner{
+
+	@Autowired
+	private MembreRepository membreRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(NjanguiProApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		this.membreRepository.save(new Membre(0,"Bart","bart@yahoo.fr"));
+		this.membreRepository.save(new Membre(0,"Lisa","lisa@yahoo.fr"));
+		this.membreRepository.save(new Membre(0,"Omer","omer@gmail.com"));
 	}
 
 }
